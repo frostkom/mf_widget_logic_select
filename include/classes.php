cf599abd8bf7869e10511dc033e1991c2aaa1230
frostkom <martin.fors@frostkom.se>
2020-06-10 12:09:12 +0200

    Make sure that JS only is loaded on the widgets page

--- include/classes.php
+++ include/classes.php
@@ -130,11 +130,16 @@ class mf_widget_logic_select
 
 	function admin_init()
 	{
-		$plugin_include_url = plugin_dir_url(__FILE__);
-		$plugin_version = get_plugin_version(__FILE__);
+		global $pagenow;
 
-		mf_enqueue_style('style_wls', $plugin_include_url."style_wp.css", $plugin_version);
-		mf_enqueue_script('script_wls', $plugin_include_url."script_wp.js", $plugin_version);
+		if($pagenow == 'widgets.php')
+		{
+			$plugin_include_url = plugin_dir_url(__FILE__);
+			$plugin_version = get_plugin_version(__FILE__);
+
+			mf_enqueue_style('style_wls', $plugin_include_url."style_wp.css", $plugin_version);
+			mf_enqueue_script('script_wls', $plugin_include_url."script_wp.js", $plugin_version);
+		}
 	}
 
 	function widget_update_callback($instance, $new_instance, $this_widget)
